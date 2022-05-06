eed2d377a991a91c84faaba5b821a9d9c9d1e338
denaalgamal <105025039+denaalgamal@users.noreply.github.com>
2022-05-06 02:24:06 +0200

    Add files via upload

---
+++ F1.js
@@ -0,0 +1,88 @@
+function Function2() {
+document.getElementById("fff").innerHTML = "It is often observed that term information system and information technology are used interchangeably. In a literal sense, information technology is a subset of information systems. Information systems consist of people, processes, machines and information technology. The great advancement in information systems is due to development in information technology and introduction of computers.";
+}
+function Function() {
+ document.open();
+ document.write("<h1> EX:- </h1> <p> An example is an online air ticket booking system, that collects data from users and generates an airline ticket and a bill. Typical organizational departments that use transaction processing systems are sales, account, finance, plant, engineering, human resource, and marketing. </p> ");
+ document.close();
+}
+
+function Function8() {
+ alert("BIS is a credit-hour at Tanta University");
+}
+
+const x=16;
+{
+constx= 10;
+document.getElementById("ppp").innerHTML ="x is " +x;
+}
+//document.getElementById("ppp").innerHTML ="x is "+x;
+
+function f() {
+  var a = 10;
+  console.log(a)
+ }
+let c =10;
+ function n() {
+ let t = 9
+  console.log(t);
+  console.log(c);
+ }
+function l() {
+  let text= "";
+  let i=0;
+  while(i < 17)
+ {
+  text+="<br> The hour is " + i;
+   i++;
+  }
+   document.getElementById("4A").innerHTML= text;
+ }
+function r()
+  {
+    let day;
+    switch (new Date().getDay()){
+    case 0:
+       day = "Sunday";
+       break;
+    case 1:
+       day = "Monday";
+       break;
+    case 2:
+       day = "Tuesday";
+       break;
+    case 3:
+       day = "Wednesday";
+       break;
+    case 4:
+       day = "Thursday";
+       break;
+    case 5:
+       day = "Friday";
+       break;
+    case 6:
+       day = "Saturday";
+ }
+  document.getElementById("dode").innerHTML="Today is " + day;
+}
+function w() {
+const time = new Date().getHours();
+let greeting;
+if (time < 10)
+  {
+    greeting = "Good morning";
+  }
+else if (time < 20)
+  {
+    greeting = "Good day";
+  }
+else {
+     greeting = "Good evening";
+    }
+document.getElementById("do").innerHTML= greeting;
+}
+        
+    
+
+
+
